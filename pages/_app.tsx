6dc6cc7bb45949722f9aa3cf6b9df3abcee847a1
J.P. Given <jgiven@gmail.com>
2022-11-17 06:16:27 -0800

    Added the Mantine Theme Provider adding font-family and color scheme.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { MantineProvider, Button } from '@mantine/core';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      theme={{
+        fontFamily:
+          '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+        colorScheme: 'dark',
+      }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
