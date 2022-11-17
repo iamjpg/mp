ace3cd8a0f28c498143a4a7918325cb161a55087
J.P. Given <jgiven@gmail.com>
2022-11-17 06:38:32 -0800

    Mapping the companies.

--- components/companies.tsx
+++ components/companies.tsx
@@ -1,7 +1,19 @@
 import { Company } from '../types/company';
+import { Card } from '@mantine/core';
 
-const Companies = ({ data }: { data: Array<Company> }) => {
-  return <>Hello from companies component.</>;
+const Companies = ({ companies }: { companies: Array<Company> }) => {
+  console.log(companies);
+  return (
+    <>
+      {companies.map((company: Company, index: number) => {
+        return (
+          <Card shadow='sm' p='lg' radius='md' mt={10} withBorder>
+            I'm card # {index}
+          </Card>
+        );
+      })}
+    </>
+  );
 };
 
 export default Companies;
