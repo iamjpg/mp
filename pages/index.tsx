20e9dcafab3e404f62701a081e1e17a49dee92cd
J.P. Given <jgiven@gmail.com>
2022-11-17 06:21:04 -0800

    Basic page layout work + the intial grabbing of data done with getStaticProps. The JSON data set is returning.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Container } from '@mantine/core';
 
 export default function Home() {
   return (
@@ -11,7 +12,26 @@ export default function Home() {
         />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>...</main>
+      <Container size={500}>
+        <h1>Mini Palomar</h1>
+        <main>Content here...</main>
+      </Container>
     </>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    'https://gist.githubusercontent.com/gyermich/6ca0c6601932bae50d3c6eb75481d302/raw/416ab16e087fbc14c0a517aa8da7a9873c38dd1e/companies.json'
+  );
+
+  const companies = await res.json();
+
+  console.log(companies);
+
+  return {
+    props: {
+      companies,
+    },
+  };
+};
